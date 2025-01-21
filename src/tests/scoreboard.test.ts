cad33324f9c934b3bcc1140d02bcab7714c34b64
Adam Orłowski <adamorlowskipoland@icloud.com>
2025-01-21 18:05:45 +0100

    extend test suite for Scoreboard

--- src/tests/scoreboard.test.ts
+++ src/tests/scoreboard.test.ts
@@ -10,16 +10,51 @@ describe('Scoreboard', () => {
   it('should add a match to the scoreboard', () => {
     const scoreboard = new Scoreboard();
     scoreboard.startMatch('Team 1', 'Team 2');
-    expect(scoreboard.matches.length).toBe(1);
+    const matches = scoreboard.getSummary();
+    expect(matches.length).toBe(1);
   });
 
   it('should finish a match', () => {
     const scoreboard = new Scoreboard();
     scoreboard.startMatch('Team 1', 'Team 2');
-    expect(scoreboard.matches.length).toBe(1);
+    const matches = scoreboard.getSummary();
+    expect(matches.length).toBe(1);
 
     scoreboard.finishMatch('Team 1', 'Team 2');
-    expect(scoreboard.matches.length).toBe(0);
+    const matches = scoreboard.getSummary();
+    expect(matches.length).toBe(0);
+  });
+
+  it('updates a match score', () => {
+    const scoreboard = new Scoreboard();
+    scoreboard.startMatch('Team 1', 'Team 2');
+    scoreboard.updateScore('Team 1', 'Team 2', 1, 2);
+    const matches = scoreboard.getSummary();
+    expect(matches[0].homeTeamScore).toBe(1);
+    expect(matches[0].awayTeamScore).toBe(2);
+  });
+
+  it('should get the summary of the matches', () => {
+    const scoreboard = new Scoreboard();
+    scoreboard.startMatch('Team 1', 'Team 2');
+    scoreboard.finishMatch('Team 1', 'Team 2');
+    expect(scoreboard.getSummary()).toBeDefined();
+  });
+
+  it('should get a summary of matches sorted by total score', () => {
+    const scoreboard = new Scoreboard();
+    scoreboard.startMatch('Team 1', 'Team 2');
+    scoreboard.updateScore('Team 1', 'Team 2', 1, 2);
+
+    scoreboard.startMatch('Team 3', 'Team 4');
+    scoreboard.updateScore('Team 3', 'Team 4', 3, 4);
+
+    const [game1, game2] = scoreboard.getSummary();
+    expect(game1.totalScore()).toBe(7);
+    expect(game2.totalScore()).toBe(3);
+
+    expect(game1.describedScore()).toBe('Team 3 3 - Team 4 4');
+    expect(game2.describedScore()).toBe('Team 1 1 - Team 2 2');
   });
 
   describe('should throw and error', () => {
