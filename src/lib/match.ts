9ab58feeffc7f7cd84e3a16dbaf4a0a8ad799e2b
Adam Orłowski <adamorlowskipoland@icloud.com>
2025-01-21 17:22:50 +0100

    delete beforeEach, create a match inside test; implement Match class; extend test suite with edge case - same team

--- src/lib/match.ts
+++ src/lib/match.ts
@@ -1,1 +1,28 @@
-export class Match { };
+export class Match {
+  constructor(
+    public homeTeam: string,
+    public awayTeam: string,
+    public homeTeamScore: number = 0,
+    public awayTeamScore: number = 0,
+  ) {
+    if (homeTeam === undefined || awayTeam === undefined) {
+      throw new Error('Match must have two teams');
+    }
+
+    if (homeTeam === awayTeam) {
+      throw new Error('Home team and away team cannot be the same');
+    }
+
+    if (arguments.length > 2) {
+      throw new Error('Match must have two teams');
+    }
+  }
+
+  totalScore(): number {
+    return this.homeTeamScore + this.awayTeamScore;
+  }
+
+  describedScore(): string {
+    return `${this.homeTeam} ${this.homeTeamScore} - ${this.awayTeam} ${this.awayTeamScore}`;
+  }
+};
--- src/tests/match.test.ts
+++ src/tests/match.test.ts
@@ -1,32 +1,32 @@
-import { describe, it, expect, beforeEach } from 'vitest';
+import { describe, it, expect } from 'vitest';
 import { Match } from '../lib/match';
 
 describe('Match', () => {
-  let match: Match;
-
-  beforeEach(() => {
-    match = new Match();
-  });
-
   describe('when creating a match', () => {
 
     it('should be able to create a match with two teams', () => {
       const match = new Match('Team 1', 'Team 2');
       expect(match.homeTeam).toBe('Team 1');
       expect(match.awayTeam).toBe('Team 2');
+      expect(match.homeTeamScore).toBe(0);
+      expect(match.awayTeamScore).toBe(0);
     });
 
     describe('with invalid arguments', () => {
-      it('should not be able to create a match with no teams', () => {
+      it('should throw an error when no teams are provided', () => {
         expect(() => new Match()).toThrow();
       });
 
-      it('should not be able to create a match with one team', () => {
+      it('should throw an error when one team is provided', () => {
         expect(() => new Match('Team 1')).toThrow();
       });
 
-      it('should not be able to create a match with more than two teams', () => {
+      it('should throw an error when more than two teams are provided', () => {
         expect(() => new Match('Team 1', 'Team 2', 'Team 3')).toThrow();
+      });
+
+      it('should throw an error when the home team and away team are the same', () => {
+        expect(() => new Match('Team 1', 'Team 1')).toThrow();
       });
     });
 
