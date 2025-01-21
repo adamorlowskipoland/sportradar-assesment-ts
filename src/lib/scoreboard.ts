3a6e537d476d961fe32e80f85d80097a89a2e5f8
Adam Orłowski <adamorlowskipoland@icloud.com>
2025-01-21 18:28:46 +0100

    start implementing class Scoreboard, add startMatch

--- src/lib/scoreboard.ts
+++ src/lib/scoreboard.ts
@@ -1,1 +1,25 @@
-export class Scoreboard { };
+import { Match } from './match';
+
+const isSameMatch = (match: Match, homeTeam: string, awayTeam: string): boolean => {
+  return match.homeTeam === homeTeam && match.awayTeam === awayTeam;
+};
+
+export class Scoreboard {
+  private matches: Match[] = [];
+
+  private isGameInProgress(homeTeam: string, awayTeam: string): boolean {
+    return this.matches.some(match => isSameMatch(match, homeTeam, awayTeam) || isSameMatch(match, awayTeam, homeTeam));
+  }
+
+  startMatch(homeTeam: string, awayTeam: string): void {
+    if (this.isGameInProgress(homeTeam, awayTeam)) {
+      throw new Error('Match already exists');
+    }
+
+    if (arguments.length !== 2) {
+      throw new Error('Match must have two teams');
+    }
+
+    this.matches.push(new Match(homeTeam, awayTeam));
+  }
+};
